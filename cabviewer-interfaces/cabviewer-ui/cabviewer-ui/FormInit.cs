f753b836e16093400e863524b7e05730401c94f7
ZHANG Muhua <31773545+TSDArthur@users.noreply.github.com>
2020-06-07 01:35:43 +0200

    update file structure.
    update file structure.

---
+++ cabviewer-interfaces/cabviewer-ui/cabviewer-ui/FormInit.cs
@@ -0,0 +1,26 @@
+﻿using CabViewerResources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CabViewerUI
+{
+    public partial class FormInit : Form
+    {
+        public FormInit()
+        {
+            InitializeComponent();
+        }
+
+        private void FormInit_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = (Image)CabViewerResourcesManager.GetResourceByName("Image_CabViewer_UI_Init_Background");
+        }
+    }
+}
---
+++ cabviewer-sdk/sdk_network/fleck/FrameType.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CabViewerSDKWebsocket
+{
+    public enum FrameType : byte
+    {
+        Continuation,
+        Text,
+        Binary,
+        Close = 8,
+        Ping = 9,
+        Pong = 10,
+    }
+}
+
