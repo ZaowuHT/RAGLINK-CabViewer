d45b178bca34bc336aa7e9b8ef740bc2c4838fff
ZHANG Muhua <31773545+TSDArthur@users.noreply.github.com>
2020-06-08 22:41:30 +0200

    updated init background.
    updated init background.

--- cabviewer-interfaces/cabviewer-ui/cabviewer-ui/Program.cs
+++ cabviewer-interfaces/cabviewer-ui/cabviewer-ui/Program.cs
@@ -16,7 +16,7 @@ namespace CabViewerUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormInit());
         }
     }
 }
